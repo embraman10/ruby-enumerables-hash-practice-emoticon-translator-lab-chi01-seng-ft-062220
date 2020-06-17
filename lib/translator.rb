bcacd93e0f364a7f6557ebf40c5aa637194a21e0
Emily Braman <embraman10@gmail.com>
2020-06-17 18:43:05 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require "yaml"
 require "pry"
 def load_library(file_path)
@@ -43,3 +44,35 @@ def get_japanese_emoticon(file_path, eng_emo)
       return translation
     end
 end
+=======
+require 'yaml'
+
+def load_library(file_path)
+  library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    library["get_emoticon"][english] = japanese
+    library["get_meaning"][japanese] = meaning
+  end
+  library
+end
+
+def get_japanese_emoticon(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
+
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+  result = library["get_meaning"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
+>>>>>>> 3d0ef3e52dafab871d938b31e76f3c019f67a390
